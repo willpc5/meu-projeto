be848fbe02a46ce152c8071749b679add1d6ec4f
Willams Silva <willamssilvainfo@gmail.com>
2026-05-29 22:32:33 -0300

    Manipulando a DOM com o JavaScript.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
 const form = document.querySelector('#form');
+const nome = document.querySelector('#nome');
+const mensagem = document.querySelector('#mensagem');
+const resultado = document.querySelector('#resultado');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const nome = document.querySelector('#nome').value;
+    const nomeUsuario = nome.value.trim();
+    const mensagemDigitada = mensagem.value.trim();
 
-    console.log(`Olá, ${nome}! Obrigado por entrar em contato. Em breve, responderei sua mensagem.`);
+    if (nomeUsuario === '' || mensagemDigitada === '') {
+        resultado.textContent = 'Por favor, preencha ambos os campos.';
+        resultado.classList.remove('text-green-600');
+        resultado.classList.add('text-red-600');
+        return;
+    }
+
+    resultado.innerHTML = `Formulário enviado com sucesso!<br>
+                           Nome: ${nomeUsuario}<br>
+                           Mensagem: ${mensagemDigitada}`;
+    resultado.classList.remove('text-red-600');
+    resultado.classList.add('text-green-600');
+                           
 });
+
+form.addEventListener('reset', function() {
+    resultado.textContent = '';
+
+    resultado.classList.remove('text-green-600', 'text-red-600');
+})
