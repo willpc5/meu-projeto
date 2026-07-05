428d93cf72bcc6e7ac0557861381b9ac9b08971d
Willams Silva <willamssilvainfo@gmail.com>
2026-07-05 00:01:18 -0300

    Atualizando arquivos

--- backend/src/prismaClient.ts
+++ backend/src/prismaClient.ts
@@ -6,11 +6,3 @@ dotenv.config();
 const prisma = new PrismaClient();
 
 export default prisma;
-
-/*
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default prisma;
-*/
