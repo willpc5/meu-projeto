19257c81b598198c6d26f435bfe1a762825194b3
Willams Silva <willamssilvainfo@gmail.com>
2026-07-31 23:46:13 -0300

    atualizando arquivo server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,41 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
+import session from 'express-session';
 import routes from './routes/routes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:8080'],
+  credentials: true
+}));
+
 app.use(express.json());
+
+app.use(session({
+  secret: 'sua_chave_secreta_aqui_123',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, 
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 2 // 2 horas
+  }
+}));
+
+// Tipagem da sessão do Express
+declare module 'express-session' {
+  interface SessionData {
+    usuarioId: string;
+  }
+}
 
 app.use(routes);
 
 app.get('/', (req: Request, res: Response) => {
-    res.json('Servidor ok');
+  res.json('Servidor ok');
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando no link http://localhost:3000');
-})
+  console.log('Servidor rodando em http://localhost:3000');
+});
